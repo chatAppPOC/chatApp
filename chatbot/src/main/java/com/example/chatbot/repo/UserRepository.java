ea3fd53b5417d53ef6f3c86bef0265a0effadcd3
Deepak PS <mr.deepakps@gmail.com>
2024-12-31 16:46:58 +0530

    Fixed case creation error

--- chatbot/src/main/java/com/example/chatbot/repo/UserRepository.java
+++ chatbot/src/main/java/com/example/chatbot/repo/UserRepository.java
@@ -10,7 +10,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
 	@Query(value = """
 			SELECT u.id, u.first_name, u.last_name, u.preferred_language, u.platform, u.title FROM users u
-			 WHERE ARRAY_CONTAINS(u.preferred_language, ?) AND ARRAY_CONTAINS(u.platform, ?) AND ARRAY_CONTAINS(u.title, ?) LIMIT 1
+			WHERE ?1 = ANY(u.preferred_language) AND ?2 = ANY(u.platform) AND ?3 = ANY(u.title) LIMIT 1
 			""", nativeQuery = true)
 	User fetchUserByLanguageAndPlatformAndTitle(@Param("language") Long language, @Param("platform") Long platform,
 			@Param("title") Long title);
