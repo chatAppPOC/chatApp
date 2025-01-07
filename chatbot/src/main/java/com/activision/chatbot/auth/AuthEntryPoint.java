6c0828a09af38ed4de42bd9a5285ec242997364a
hareesh.kumar <hareesh.kumar@activision.com>
2025-01-07 23:25:32 +0530

    Adding basic auth instead of token cookie based and cors config

---
+++ chatbot/src/main/java/com/activision/chatbot/auth/AuthEntryPoint.java
@@ -0,0 +1,33 @@
+package com.activision.chatbot.auth;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class AuthEntryPoint implements AuthenticationEntryPoint, Serializable {
+
+	private static final long serialVersionUID = -7858869558953243875L;
+
+	/**
+	 *@author Hareesh
+	 *@implNote Executed mostly if unauthorized
+	 *
+	 *@param HttpServletRequest, HttpServletResponse, AuthenticationException
+	 */
+	@Override
+	public void commence(HttpServletRequest request, HttpServletResponse response,
+			AuthenticationException authException) throws IOException {
+		
+		//Setting unauthorized response
+		response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+		response.setContentType("application/json;charset=UTF-8");
+		response.getWriter().write("Incorrect Credentials");
+	}
+}
