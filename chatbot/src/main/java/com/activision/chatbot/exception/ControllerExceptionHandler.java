3a7cdd32f63d0ac6c77383b50dcadb3dee8ace46
Deepak PS <Deepak.Sudhakar@activision.com>
2025-01-15 20:00:08 +0530

    Update ControllerExceptionHandler.java

--- chatbot/src/main/java/com/activision/chatbot/exception/ControllerExceptionHandler.java
+++ chatbot/src/main/java/com/activision/chatbot/exception/ControllerExceptionHandler.java
@@ -15,7 +15,7 @@ public class ControllerExceptionHandler extends ResponseEntityExceptionHandler{
 	@ExceptionHandler(UniqueConstraintViolationException.class)
 	public ResponseEntity<Object> handleDuplicateNames(UniqueConstraintViolationException ex) {
 	    Map<String, Object> body = new LinkedHashMap<>();
-	    body.put("message", "Duplicate names are not allowed");
+	    body.put("message", "A content with the same name and language already exists. Please try with a different name or language.");
 	    return new ResponseEntity<>(body, HttpStatus.CONFLICT);
 	}
 }
